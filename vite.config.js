6d56c4e7313af4d846137a1be95057e280f5230a
hannah <hannah@topologic.io>
2026-03-23 16:25:48 -0400

    consolidate the tools

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        structure: resolve(__dirname, "structure/index.html"),
+        panel: resolve(__dirname, "panel/index.html"),
+      },
+    },
+  },
+});
