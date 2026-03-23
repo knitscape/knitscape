4db63f3bd6655031e30b29aa5a7d3ad9f0cb21d9
hannah <hannah@topologic.io>
2026-03-23 17:09:39 -0400

    switch to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./panel/**/*.{html,js}",
+    "./structure/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        base: {
+          0: "#0e0e0e",
+          1: "#1a1a1a",
+          2: "#2b2b2b",
+          3: "#3c3c3c",
+          4: "#4d4d4d",
+          5: "#5e5e5e",
+          6: "#6f6f6f",
+          7: "#8a8a8a",
+          8: "#9b9b9b",
+          9: "#a1a1a1",
+          10: "#b2b2b2",
+          11: "#c3c3c3",
+          12: "#d4d4d4",
+          13: "#e5e5e5",
+        },
+        accent: "hsla(316, 29%, 64%)",
+      },
+      fontFamily: {
+        "national-park": ['"National Park"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
